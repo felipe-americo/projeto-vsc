56149efa37c0df9908c301e2514b18b73e24db17
felipe-americo <americo.felipe@escola.pr.gov.br>
2026-03-24 11:05:22 -0300

    projeto vsc
    projeto vsc

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 document.querySelector(#p).textContent= "index.html";
 let frases = ["F1", "F2", "F3"];
+let frasesCopiada = [...frases];
+
 function geradorMensagem(){
     if(frases.length == 0){
         frases = [...frasesCopiada];
     }
 let indiceGerado = Math.floor(Math.random()*(frases.length));
 let fraseGerada = frases[indiceGerado];
-console.log(fraseGerada)
+
+frases.splice(indiceGerado,1)
 }
 geradorMensagem();
-let frasesCopiada = [...frases];
